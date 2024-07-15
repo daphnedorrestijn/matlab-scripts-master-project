324cb3597a5a940fc7a2a8fbccb10b79f2d2ee9c
20182270 <20182270@20182270.campus.tue.nl>
2024-07-15 10:19:24 +0200

    Oude versies er nog bij

--- Matlab scripts/20240408/Data_aanpassen.m
+++ Matlab scripts/20240408/Data_aanpassen.m
@@ -27,7 +27,7 @@ clc
 
 % Import the data
 % BasleracA204090uc21599070202404081112386120001diff = readtable("C:\Users\20182270\OneDrive - TU Eindhoven\Documents\0 Graduation project\Matlab\Output\20240417\Results try 1\Basler_acA2040-90uc__21599070__20240417_105824006_0018_diff.dat", opts);
-file_original = readtable("C:\Users\20182270\OneDrive - TU Eindhoven\Documents\0 Graduation project\Matlab\Input\20240611_new_A3\20240611_nonrot_2.4Hz_8mm_new_A3_separate_frames\Batch processing\Results\20240611_nonrot_2.4Hz_8mm_new_A3_frame_0010A_vel.dat");
+file_original = readtable("C:\Users\20182270\OneDrive - TU Eindhoven\Documents\0 Graduation project\Matlab\Input\20240618_new_A3\20240618_rot_2Hz_5mm_new_A3_separate_frames\Results 2\20240618_rot_2Hz_5mm_new_A3_frame_0134A_vel.dat");
 
 % clear opts
 
@@ -48,7 +48,7 @@ data_new = file_original(:,[1 2 4:14]);
 
 %sprintf('%.0f',data_new(:,[1 2]))
 %writematrix(data_new,"C:\Users\20182270\OneDrive - TU Eindhoven\Documents\0 Graduation project\Matlab\Output\20240417\Results try 1\Basler_acA2040-90uc__21599070__20240417_105824006_0018_diff_new.dat")
-writetable(data_new,"C:\Users\20182270\OneDrive - TU Eindhoven\Documents\0 Graduation project\Matlab\Input\20240611_new_A3\20240611_nonrot_2.4Hz_8mm_new_A3_separate_frames\Batch processing\Results\20240611_nonrot_2.4Hz_8mm_new_A3_frame_0010A_vel_new.dat")
+writetable(data_new,"C:\Users\20182270\OneDrive - TU Eindhoven\Documents\0 Graduation project\Matlab\Input\20240618_new_A3\20240618_rot_2Hz_5mm_new_A3_separate_frames\Results 2\20240618_rot_2Hz_5mm_new_A3_frame_0134A_vel_new.dat")
 
 %save("C:\Users\20182270\OneDrive - TU Eindhoven\Documents\0 Graduation project\Camera images\Basler_acA2040-90uc__21599070__20240408_111238612_0001_diff_new_2.dat", "data_new",'-ascii');
 %"C:\Users\20182270\OneDrive - TU Eindhoven\Documents\0 Graduation project\Camera images\Basler_acA2040-90uc__21599070__20240408_111238612_0001_diff_new_2.dat" = sprintf('output=%d.dat', data_new);
